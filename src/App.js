48153cdd843eecad4e8449a7994f8847fd9af3f6
Ortal Sucharevich <ortalsucharevich@Ortals-MacBook-Air.local>
2021-09-06 11:28:54 +0300

    remove server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Pad from './components/pad/Pad.js';
-import axios from 'axios';
 import Button from './components/Button/Button';
 
 
+const pads  = [
+  {
+      "name": "future funk beats",
+      "color": "red", 
+      "url": "padsLinks/120_future_funk_beats_25.mp3"
+  },
+  {
+      "name": "stutter breakbeats",
+      "color": "green", 
+      "url": "padsLinks/120_stutter_breakbeats_16.mp3"
+  },
+  {
+      "name": "Bass Warwick heavy funk groove ",
+      "color": "yellow", 
+      "url": "padsLinks/Bass Warwick heavy funk groove on E 120 BPM.mp3"
+  },
+  {
+      "name": "electric guitar coutry slide",
+      "color": "blue", 
+      "url": "padsLinks/electric guitar coutry slide 120bpm - B.mp3"
+  },
+  {
+      "name": "StompySlosh",
+      "color": "grey", 
+      "url": "padsLinks/FUD_120_StompySlosh.mp3"
+  },
+  {
+      "name": "GrooveB",
+      "color": "white", 
+      "url": "padsLinks/GrooveB_120bpm_Tanggu.mp3"
+  },
+  {
+      "name": "MazePolitics",
+      "color": "purple", 
+      "url": "padsLinks/MazePolitics_120_Perc.mp3"
+  },
+  {
+      "name": "PAS3GROOVE",
+      "color": "pink", 
+      "url": "padsLinks/PAS3GROOVE1.03B.mp3"
+  },
+  {
+      "name": "SilentStar",
+      "color": "brown", 
+      "url": "padsLinks/SilentStar_120_Em_OrganSynth.mp3"
+  }
+];
 
 function App() {
   const [isRunning, setIsRunning] = useState(false);
@@ -24,16 +70,12 @@ function App() {
   }
 
   useEffect(() => {
-    axios.get('http://localhost:8080/pads')
-        .then(res => {
-            let data = res.data;
-            for (let i = 0; i < data.length; i++) {
-              data[i].url = 'http://localhost:8080/' + data[i].url;
-              data[i].audio = new Audio(data[i].url);
-              data[i].audio.load();
-            }
-            setLoops(data);
-        });
+    for (let i = 0; i < pads.length; i++) {
+      const element = pads[i];
+      element.audio = new Audio(element.url);
+      element.audio.load();
+    }
+    setLoops(pads);console.log(pads);
   }, []);
 
   // play all pads that select
